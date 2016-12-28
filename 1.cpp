66149884e27bd66eca9f4e7d27bc360eab60c2c1
gx_wind <wind.guoxing@gmail.com>
2016-12-28 22:33:17 +0800

    two sum

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namesapce std;
+
+vector<int> twoSum(vector<int>& nums, int target);
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> res;
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i+1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target) {
+                res.push_back(i);
+                res.push_back(j);
+                return res;
+            }
+        }
+    }
+    return res;
+}
+
+int main() {
+    return 0;
+}
