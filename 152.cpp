93305b5f6dc1867075ab671299951d19f80dee33
gx_wind <guoxing@baidu.com>
2017-05-14 16:15:53 +0800

    add problem 152

---
+++ 152.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+    if (nums.size() == 0) {
+        return 0;
+    }
+    int sum, imin, imax = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] < 0) {
+            swap(imin, imax);
+        }
+        imin = min(imin, nums[i]*imin);
+        imax = max(imax, nums[i]*imax);
+
+        sum = max(sum, imax);
+    }
+    return sum;
+}
+
+int main() {
+    vector<
+    return 0;
+}
+
