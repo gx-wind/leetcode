c5c3508c865cea109fad919237b301f00d9ad7aa
gx_wind <guoxing@baidu.com>
2017-05-21 16:31:37 +0800

    add problem 122

---
+++ 122.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int res = 0;
+    for (int i = 1; i < prices.size(); i++) {
+        if (prices[i] - prices[i-1] > 0) {
+            res += prices[i] - prices[i-1];
+        }
+    }
+    return res;
+}
+
+int main() {
+    return 0;
+}
