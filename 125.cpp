ed6f011f8e2c39850f01d4d12d3e6b3088a26ccf
gx_wind <guoxing@baidu.com>
2017-05-21 17:06:32 +0800

    add problem 125

---
+++ 125.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+bool isPalindrome(string s) {
+    if (s.size() == 0) {
+        return true;
+    }
+    int i = 0, j = s.size()-1;
+    while (i <= j) {
+        while ((!isalpha(s[i])) && (!isdigit(s[i]))) {
+            i++;
+        }
+        while ((!isalpha(s[j])) && (!isdigit(s[j]))){
+            j--;
+        }
+        s[i] = tolower(s[i]);
+        s[j] = tolower(s[j]);
+        cout << i << " " << s[i] << endl;
+        cout << j << " " << s[j] << endl;
+        if (i <= j) {
+            if (s[i] != s[j]) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    isPalindrome("Live on evasions? No, I save no evil.");
+    return 0;
+}
