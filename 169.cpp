0e5948746e4bbd7ea1661672d9341fd7fa55a0f2
gx_wind <wind.guoxing@gmail.com>
2017-06-10 22:50:55 +0800

    add problem 169

---
+++ 169.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    return nums[nums.size()/2];
+}
+
+int main() {
+    return 0;
+}
