88a3f3915c12606286cde002793d1a4e13579f94
gx_wind <wind.guoxing@gmail.com>
2017-06-10 16:57:03 +0800

    add problem 437

---
+++ 437.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int depSearch(TreeNode* root, int pre, int& sum) {
+    if (!root) {
+        return 0;
+    }
+    int current = pre + root->val;
+    return (current == sum) + depSearch(root->left, current, sum) + depSearch(root->left, current, sum);
+}
+
+int pathSum(TreeNode* root, int sum) {
+    if (!root) {
+        return 0;
+    }
+    int pre = 0;
+    return depSearch(root, pre, sum) + pathSum(root->left) + pathSum(root->right);
+}
+
+int main() {
+    return 0;
+}
