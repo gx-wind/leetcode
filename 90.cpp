6ee978a3a36456757693af643be10776b4630304
gx_wind <wind.guoxing@gmail.com>
2017-06-11 22:13:17 +0800

    add problem 90

--- 90.cpp
+++ 90.cpp
@@ -12,7 +12,7 @@ void depSearch(vector<vector<int> >& res, vector<int>& v, vector<int>& nums, int
     for (int i = index; i < nums.size(); i++) {
         if (i == index || nums[i] != nums[i-1]) {
             v.push_back(nums[i]);
-            depSearch(res, v, nums, index+1, depth);
+            depSearch(res, v, nums, i+1, depth);
             v.pop_back();
         }
     }
@@ -21,8 +21,7 @@ void depSearch(vector<vector<int> >& res, vector<int>& v, vector<int>& nums, int
 vector<vector<int> > subsetsWithDup(vector<int>& nums) {
     sort(nums.begin(), nums.end());
     vector<vector<int> > res;
-    res.push_back(nums);
-    for (int i = 0; i < nums.size(); i++) {
+    for (int i = 0; i <= nums.size(); i++) {
         vector<int> v;
         depSearch(res, v, nums, 0, i);
     }
