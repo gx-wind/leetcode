987b55fc9da95941422bed01a328d338a05880c4
gx_wind <wind.guoxing@gmail.com>
2017-06-11 21:28:13 +0800

    add problem 90

---
+++ 90.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void depSearch(vector<vector<int> >& res, vector<int>& v, vector<int>& nums, int index, int depth) {
+    if (depth == v.size()) {
+        res.push_back(v);
+        return;
+    }
+    for (int i = index; i < nums.size(); i++) {
+        if (i == index || nums[i] != nums[i-1]) {
+            v.push_back(nums[i]);
+            depSearch(res, v, nums, index+1, depth);
+            v.pop_back();
+        }
+    }
+}
+
+vector<vector<int> > subsetsWithDup(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int> > res;
+    res.push_back(nums);
+    for (int i = 0; i < nums.size(); i++) {
+        vector<int> v;
+        depSearch(res, v, nums, 0, i);
+    }
+    return res;
+}
+
+int main() {
+    return 0;
+}
