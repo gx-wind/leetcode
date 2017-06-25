733ff0a834702ee2a1b575ca6cf22b7116fdeb12
gx_wind <wind.guoxing@gmail.com>
2017-06-25 23:14:53 +0800

    add problem 442

---
+++ 442.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> findDuplicates(vector<int>& nums) {
+    vector<int> v;
+    for (int i = 0; i < nums.size(); i++) {
+        int val = abs(nums[i])-1;
+        if (nums[val] < 0) {
+            v.push_back(abs(nums[i]));
+        } else {
+            nums[val] = -nums[val];
+        }
+    }
+    return v;
+}
+
+int main() {
+    return 0;
+}
